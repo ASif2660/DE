3772fb2ba836573745ea45d760ad691ced2e4169
ASif2660 <mdasifchandchand@gmail.com>
2019-09-05 13:10:28 +0200

    Everything again

--- main.cpp
+++ main.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
 
+
+/*
+ * Program developed by CANM
+ * Contact person: Ergun Yavuz
+ * email:  Ergun.Yavuz@ServiceXpert.de and Mohammed.Chand@ServiceXpert.de
+ *
+ */
+
+
+/* TODO: include all the headers of the SDK
+ * TODO: include all the headers of ARUCO
+ * TODO: locate the ARUCO Marker with a test program
+ * TODO: locate the average depth of a bounded region using the SDK functions
+ * TODO: Integrate both the functions
+ * TODO: Tracking ?
+ *
+ * /
+
+
+
+
+ */
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
