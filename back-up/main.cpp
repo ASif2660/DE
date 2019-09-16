def0b24ef10e928edbff75236811f3aecb43b5c4
ASif2660 <mdasifchandchand@gmail.com>
2019-09-16 17:16:51 +0200

    full

---
+++ back-up/main.cpp
@@ -0,0 +1,169 @@
+
+#include "sl/Camera.hpp"
+#include "opencv2/aruco.hpp"
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
+#include "zed2cv.h"
+#include <iostream>
+#include <opencv2/imgproc/imgproc.hpp>
+
+
+
+
+
+#include <typeinfo>
+
+/*
+ * Program developed by CANM
+ * Contact person: Ergun Yavuz
+ * email:  Ergun.Yavuz@ServiceXpert.de and Mohammed.Chand@ServiceXpert.de
+ *
+ */
+
+
+/* TODO: DONE
+ * TODO: include all the headers of ARUCO
+ * TODO: locate the ARUCO Marker with a test program
+ * TODO: locate the average depth of a bounded region using the SDK functions
+ * TODO: Integrate both the functions
+ * TODO: cv::Mat to sl::Mat conversion
+ * TODO: Tracking ?
+ *
+ * /
+
+ */
+
+
+int main(int argc, char* argv[] ) {
+
+    //initialize the Configuration parameters of the ZED Camera
+
+    sl::InitParameters init_params;
+  //  init_params.enable_right_side_measure = true;
+    init_params.depth_mode = sl::DEPTH_MODE_ULTRA;
+    init_params.coordinate_units = sl::UNIT_MILLIMETER;
+
+
+
+    std::vector<int> markerIds;
+    std::vector<std::vector<cv::Point2f>> markerCorners;
+    cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
+
+
+
+
+    // Define the Camera Object and related data types
+    sl::Camera zed;
+    sl::ERROR_CODE zed_camera_error = zed.open(init_params);
+    sl::Mat depth_image(zed.getResolution(), sl::MAT_TYPE_8U_C1); // this is going to be an RGBA image
+    sl::Mat image_full(zed.getResolution(), sl::MAT_TYPE_8U_C4);
+    sl::Mat depth_calculate(zed.getResolution(), sl::MAT_TYPE_32F_C1);
+    float depth_center_pixel =0;
+    cv::int16_t x = 0;
+    cv::int16_t y = 0;
+
+
+
+
+for (int k = 0; k < 100; k++ ) {
+
+
+    if (zed.grab() == sl::SUCCESS) { //1280x720
+
+
+
+
+        zed.retrieveImage(depth_image, sl::VIEW_LEFT);
+        zed.retrieveImage(image_full, sl::VIEW_LEFT);
+        zed.retrieveMeasure(depth_calculate, sl::MEASURE_DEPTH);
+
+
+        // Convert ZED Camera Mat objects to CV::Mat Objects
+        cv::Mat cv_depth_image = slMat2cvMat(depth_image);
+        cv::Mat cv_RGB_image = slMat2cvMat(image_full);
+
+        cvtColor(cv_depth_image, cv_depth_image, CV_8UC1);
+        cvtColor(cv_RGB_image, cv_RGB_image,CV_RGBA2RGB);
+
+
+  /*
+        float depth = cv_depth_image.at<float>(cv_depth_image.rows / 2, cv_depth_image.cols / 2);
+
+
+        std::cout << " The depth of centermost pixel is " << depth << std::endl;
+
+        std::string value = "Depth value " + std::to_string(depth);
+
+*/
+         cv::Mat cv_RGB_image_copy;
+         cv_RGB_image.copyTo(cv_RGB_image_copy);
+         cv::aruco::detectMarkers(cv_RGB_image, dictionary, markerCorners, markerIds);
+
+
+         if( markerIds.size() > 0) {
+
+
+             //       std::cout << " Found the marker " << std::endl;
+
+
+
+             cv::aruco::drawDetectedMarkers(cv_RGB_image_copy, markerCorners, markerIds);
+
+
+
+
+             for (std::vector<int>::size_type i = 0; i < markerCorners.size(); i++) {
+
+                 for (std::vector<int>::size_type j = 0; j < markerCorners[i].size(); j++) {
+
+//                    std::cout << "Four Corners are " << markerCorners[i][j].x << typeid(markerCorners[i][j].x).name() << std::endl;
+
+
+                     x += markerCorners[i][j].x;
+
+                     y += markerCorners[i][j].y;
+
+
+                     std::cout << " Inner for loop " << "x  and y" << x << " " << y << std::endl;
+
+                 }
+
+             }
+
+             x = int(x) / 4;  //calculating the moments or centroid
+             y = int(y) / 4;
+
+
+         }
+
+         // we are basically saying we have enough ids inside the vector of the markerIds
+
+            std::cout << "Thjs raw value is " << x  <<  "and " << y << std::endl;
+
+
+            if (x < 1280 && y < 720 ) {
+
+             //   depth_calculate.getValue(y,x,&depth_center_pixel)
+
+                std::cout <<  "The depth is " << depth_center_pixel << std::endl;
+
+         }
+
+
+        cv::namedWindow("ARUCO_RGB", CV_WINDOW_AUTOSIZE);
+
+        cv::imshow( "ARUCO_RGB ", cv_RGB_image_copy);
+
+        cv::waitKey(200);
+
+
+     } else sl::sleep_ms(10);
+
+    } //grabber loop end
+
+
+    cv::destroyAllWindows();
+
+    return 0;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,177 +1,266 @@
+///////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2019, STEREOLABS.
+//
+// All rights reserved.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+///////////////////////////////////////////////////////////////////////////
 
-#include "sl/Camera.hpp"
+/***********************************************************************************************
+ ** This sample demonstrates how to reloc a ZED camera using an ArUco marker.                  **
+ ** Images are captured with the ZED SDK and cameras poses is then computed from ArUco pattern **
+ ** to reset ZED tracking with this known position.                                            **
+ ***********************************************************************************************/
+
+// ZED includes
+#include <sl/Camera.hpp>
+
+// Sample includes
+//#include "aruco.hpp"
+
 #include "opencv2/aruco.hpp"
+
+// OCV includes
 #include <opencv2/opencv.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/core/core.hpp>
-#include "zed2cv.h"
-#include <iostream>
-#include <opencv2/imgproc/imgproc.hpp>
 
+using namespace sl;
+using namespace std;
 
+int main(int argc, char **argv) {
 
 
 
-#include <typeinfo>
+    // Create a ZED camera object
 
-/*
- * Program developed by CANM
- * Contact person: Ergun Yavuz
- * email:  Ergun.Yavuz@ServiceXpert.de and Mohammed.Chand@ServiceXpert.de
- *
- */
+    Camera zed;
 
 
-/* TODO: DONE
- * TODO: include all the headers of ARUCO
- * TODO: locate the ARUCO Marker with a test program
- * TODO: locate the average depth of a bounded region using the SDK functions
- * TODO: Integrate both the functions
- * TODO: cv::Mat to sl::Mat conversion
- * TODO: Tracking ?
- *
- * /
 
- */
+    // Set configuration parameters
 
+    InitParameters init_params;
 
-int main(int argc, char* argv[] ) {
+    init_params.camera_resolution = RESOLUTION_HD720;
 
-    //initialize the Configuration parameters of the ZED Camera
+    init_params.coordinate_units = UNIT_MILLIMETER;
 
-    sl::InitParameters init_params;
-    init_params.enable_right_side_measure = true;
-    init_params.depth_mode = sl::DEPTH_MODE_ULTRA;
-    init_params.coordinate_units = sl::UNIT_MILLIMETER;
+    init_params.camera_disable_imu = true; // for this sample, IMU (of ZED-M) is disable, we use the gravity given by the marker.
 
 
 
-    std::vector<int> markerIds;
-    std::vector<std::vector<cv::Point2f>> markerCorners;
-    cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
+    // Open the camera
 
+    ERROR_CODE err = zed.open(init_params);
 
+    if (err != SUCCESS) {
 
+        cout << "Error, unable to open ZED camera: " << err << "\n";
 
+        zed.close();
 
-    // Define the Camera Object and related data types
-    sl::Camera zed;
-    sl::ERROR_CODE zed_camera_error = zed.open(init_params);
-    sl::Mat depth_image(zed.getResolution(), sl::MAT_TYPE_8U_C1); // this is going to be an RGBA image
-    sl::Mat image_full(zed.getResolution(), sl::MAT_TYPE_8U_C4);
-    sl::Mat depth_calculate(zed.getResolution(), sl::MAT_TYPE_32F_C1);
-    float depth_center_pixel =0;
-    cv::int16_t x = 0;
-    cv::int16_t y = 0;
+        return 1; // Quit if an error occurred
 
+    }
 
 
 
-for (int k = 0; k < 100; k++ ) {
+    Resolution image_size = zed.getResolution();
 
-    if (zed.grab() == sl::SUCCESS) { //1280x720
+    Mat image_zed(image_size, MAT_TYPE_8U_C4);
 
+    cv::Mat image_ocv = cv::Mat(image_zed.getHeight(), image_zed.getWidth(), CV_8UC4, image_zed.getPtr<sl::uchar1>(MEM_CPU));
 
+    cv::Mat image_ocv_rgb;
 
-        zed.retrieveImage(depth_image, sl::VIEW_DEPTH_RIGHT);
-        zed.retrieveImage(image_full, sl::VIEW_RIGHT);
-        zed.retrieveMeasure(depth_calculate, sl::MEASURE_DEPTH);
 
 
+    auto calibInfo = zed.getCameraInformation().calibration_parameters.left_cam;
 
-        // Convert ZED Camera Mat objects to CV::Mat Objects
-        cv::Mat cv_depth_image = slMat2cvMat(depth_image);
-        cv::Mat cv_RGB_image = slMat2cvMat(image_full);
+    cv::Matx33d camera_matrix = cv::Matx33d::eye();
 
+    camera_matrix(0, 0) = calibInfo.fx;
 
+    camera_matrix(1, 1) = calibInfo.fy;
 
+    camera_matrix(0, 2) = calibInfo.cx;
 
-        cvtColor(cv_depth_image, cv_depth_image, CV_8UC1);
-        cvtColor(cv_RGB_image, cv_RGB_image,CV_RGBA2RGB);
+    camera_matrix(1, 2) = calibInfo.cy;
 
 
-  /*
 
-        float depth = cv_depth_image.at<float>(cv_depth_image.rows / 2, cv_depth_image.cols / 2);
+    cv::Matx<float, 4, 1> dist_coeffs = cv::Vec4f::zeros();
 
 
-        std::cout << " The depth of centermost pixel is " << depth << std::endl;
 
-        std::string value = "Depth value " + std::to_string(depth);
+    float actual_marker_size_meters = 0.16f; // real marker size in meters
 
-*/
-         cv::Mat cv_RGB_image_copy;
-         cv_RGB_image.copyTo(cv_RGB_image_copy);
-         cv::aruco::detectMarkers(cv_RGB_image, dictionary, markerCorners, markerIds);
+    auto dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_100);
 
 
-         if( markerIds.size() > 0) {
 
+    cout << "Make sure the ArUco marker is a 6x6 (100), measuring " << actual_marker_size_meters * 1000 << " mm" << endl;
 
-             //       std::cout << " Found the marker " << std::endl;
 
 
+    Transform pose;
 
-             cv::aruco::drawDetectedMarkers(cv_RGB_image_copy, markerCorners, markerIds);
+    Pose zed_pose;
 
+    vector<cv::Vec3d> rvecs, tvecs;
 
+    vector<int> ids;
 
+    vector<vector<cv::Point2f> > corners;
 
-             for (std::vector<int>::size_type i = 0; i < markerCorners.size(); i++) {
+    string position_txt;
 
-                 for (std::vector<int>::size_type j = 0; j < markerCorners[i].size(); j++) {
 
-//                    std::cout << "Four Corners are " << markerCorners[i][j].x << typeid(markerCorners[i][j].x).name() << std::endl;
 
+    sl::Mat point_cloud;
 
-                     x += markerCorners[i][j].x;
+    sl::float4 point_cloud_value;
 
-                     y += markerCorners[i][j].y;
 
 
-                     std::cout << " Inner for loop " << "x  and y" << x << " " << y << std::endl;
+    bool can_reset = false;
 
-                 }
 
-             }
 
-             x = int(x) / 4;
-             y = int(y) / 4;
+    zed.enableTracking();
 
 
 
+    // Loop until 'q' is pressed
 
-             exit(0);
-         }
+    char key = '.';
 
+    while (key != 'q') {
 
-             // we are basically saying we have enough ids inside the vector of the markerIds
+        if (zed.grab() == SUCCESS) {
 
-            std::cout << "Thje raw value is " << x  <<  "and " << y << std::endl;
+            // Retrieve the left image
 
+            zed.retrieveImage(image_zed, VIEW_LEFT, MEM_CPU, image_size.width, image_size.height);
 
-            if (x < 1280 && y < 720 ) {
 
-                depth_calculate.getValue(y,x,&depth_center_pixel);
 
-                std::cout <<  "The depth is " << depth_center_pixel << std::endl;
+            // convert to RGB
 
-         }
+            cv::cvtColor(image_ocv, image_ocv_rgb, cv::COLOR_RGBA2RGB);
 
+            // detect marker
 
-        cv::namedWindow("ARUCO_RGB", CV_WINDOW_AUTOSIZE);
+            cv::aruco::detectMarkers(image_ocv_rgb, dictionary, corners, ids);
 
-        cv::imshow( "ARUCO_RGB ", cv_depth_image);
 
-        cv::waitKey(200);
 
+            // get actual ZED position
 
-     } else sl::sleep_ms(10);
+            zed.getPosition(zed_pose);
 
-    } //grabber loop end
 
 
-    cv::destroyAllWindows();
+            zed.retrieveMeasure(point_cloud, MEASURE_XYZBGRA);
+
+
+
+            // display ZED position
+
+            cv::rectangle(image_ocv_rgb, cv::Point(0, 0), cv::Point(490, 75), cv::Scalar(0, 0, 0), -1);
+
+            cv::putText(image_ocv_rgb, "Loaded dictionary : 6x6.     Press 'SPACE' to reset the camera position", cv::Point(10, 15), cv::FONT_HERSHEY_SIMPLEX, 0.4, cv::Scalar(220, 220, 220));
+
+            position_txt = "ZED  x: " + to_string(zed_pose.pose_data.tx) + "; y: " + to_string(zed_pose.pose_data.ty) + "; z: " + to_string(zed_pose.pose_data.tz);
+
+            cv::putText(image_ocv_rgb, position_txt, cv::Point(10, 35), cv::FONT_HERSHEY_SIMPLEX, 0.6, cv::Scalar(236, 188, 26));
+
+
+
+            // if at least one marker detected
+
+            if (ids.size() > 0) {
+
+                cv::aruco::estimatePoseSingleMarkers(corners, actual_marker_size_meters, camera_matrix, dist_coeffs, rvecs, tvecs);
+
+                pose.setTranslation(sl::float3(tvecs[0](0), tvecs[0](1), tvecs[0](2)));
+
+                pose.setRotationVector(sl::float3(rvecs[0](0), rvecs[0](1), rvecs[0](2)));
+
+                pose.inverse();
+
+                can_reset = true;
+
+
+
+                cv::aruco::drawDetectedMarkers(image_ocv_rgb, corners, ids);
+
+                cv::aruco::drawAxis(image_ocv_rgb, camera_matrix, dist_coeffs, rvecs[0], tvecs[0], actual_marker_size_meters * 0.5f);
+
+                //position_txt = "Aruco x: " + to_string(pose.tx) + "; y: " + to_string(pose.ty) + "; z: " + to_string(pose.tz);
+
+                int x = (corners[0][0].x + corners[0][1].x + corners[0][2].x + corners[0][3].x) / 4;
+                int y = (corners[0][0].y + corners[0][1].y + corners[0][2].y + corners[0][3].y) / 4;
+
+                point_cloud.getValue(x, y, &point_cloud_value);
+
+
+
+                float distance = sqrt(point_cloud_value.x * point_cloud_value.x + point_cloud_value.y * point_cloud_value.y + point_cloud_value.z * point_cloud_value.z);
+
+
+
+                position_txt = "X: " + to_string(x) + " Y: " + to_string(y) +  " Distance is: " + to_string(distance);
+
+
+
+                cv::putText(image_ocv_rgb, position_txt, cv::Point(10, 60), cv::FONT_HERSHEY_SIMPLEX, 0.6, cv::Scalar(124, 252, 124));
+
+
+
+            } else
+
+                can_reset = false;
+
+
+
+            // Display image
+
+            cv::imshow("Image", image_ocv_rgb);
+
+
+
+            // Handle key event
+
+            key = cv::waitKey(10);
+
+
+
+            // if KEY_R is pressed and aruco marker is visible, then reset ZED position
+
+            if ((key == ' ') && can_reset)
+
+                zed.resetTracking(pose);
+
+        }
+
+    }
+
+    zed.close();
 
     return 0;
+
+
+
 }
